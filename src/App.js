ab20a940ec92a7076364cfebae69a7f75a83be7d
Hungbanh1 <hungbg.dev1@gmail.com>
2022-09-28 17:21:10 +0700

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Following from './pages/Following';
+import { publicRoutes } from './Routes/index';
+import DefaultLayout from './components/Layout/DefaultLayout';
+import { Fragment } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* cach 1  */}
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+
+                    {/* cach 2 */}
+                    {/* <Route path="/" element={<Home />}></Route>
+                    <Route path="/following" element={<Following />}></Route> */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
